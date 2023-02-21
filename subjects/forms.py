31cfa3360cbbbb868c5bc356b06bce7e84fca35b
Samuel Lambert <samuel.lambert@epita.fr>
2023-02-21 16:56:42 +0000

    frontend

--- subjects/forms.py
+++ subjects/forms.py
@@ -5,10 +5,20 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
         exclude = []
-
+        widgets = {
+            'subject' : forms.Select(
+                attrs={'class': 'form-subject'}
+            ),
+            'name' : forms.TextInput(
+                attrs={'class': 'form-name'}
+            ),
+            'file' : forms.FileInput(
+                attrs={'class': 'form-file'}
+            ),
+        }
 
 
 class SubjectForm(forms.ModelForm):
     class Meta:
         model = Subject
-        exclude = []
+        exclude = ['nb_notes']
